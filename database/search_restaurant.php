f2795eced2fc2633899d29dda14829ef328954ed
filpez <joaofilipepereiracosta@hotmail.com>
2016-11-30 19:15:40 +0000

    Acho que ta corrigido

--- database/search_restaurant.php
+++ database/search_restaurant.php
@@ -26,7 +26,7 @@
         	foreach($result as $restaurant) { ?>
 				<div id="content">
 					<div class="restaurant">
-						<h3> <?=$restaurant['name']?> </h3>
+						<h3 class="restaurant_name"> <?=$restaurant['name']?> </h3>
 						<img src="http://ipsumimage.appspot.com/300x200,ff7700" alt="300x200">
 						<p class="points">Pontos: <?=getRestaurantScore($restaurant['id'])?> em 10</p>
 						<p class="place"> <?=$restaurant['local']?> </p>
--- templates/restaurant.php
+++ templates/restaurant.php
@@ -1,5 +1,5 @@
 <div class="restaurant">
-	<h3> <?=$restaurant['name']?> </h3>
+	<h3 class="restaurant_name"> <?=$restaurant['name']?> </h3>
 	<img src="http://ipsumimage.appspot.com/300x200,ff7700" alt="300x200">
 	<p class="points">Pontos: <?=getRestaurantScore($restaurant['id'])?> em 10</p>
 	<p class="place"> <?=$restaurant['local']?> </p>
