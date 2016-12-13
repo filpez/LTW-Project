c54ceef83becb2267e83fcc26dc10350d610a667
filpez <joaofilipepereiracosta@hotmail.com>
2016-12-13 19:25:11 +0000

    Acho que ta a dar o add image

--- database/addImage.php
+++ database/addImage.php
@@ -1,8 +1,7 @@
 <?php
 function addImage($db, $id){
-	$name = $_POST['name'];
-
-	if($id == NULL){
+	//$name = $_POST['name'];
+	/*if($id == NULL){
 		$stmt = $db->prepare("Select * From restaurant where name=:name");
 		$stmt->bindParam(':name',$name,PDO::PARAM_STR);
     	$stmt->execute();
@@ -11,12 +10,13 @@ function addImage($db, $id){
 			echo 'Error';
 		else if (count($result) == 1)
 			$id = $result[0]['id'];
-	}
+	}*/
 	$path = "uploads/";
 
 	//Check if there's a file
 	$imgname = $_FILES['photo']['name'];
 	if(!$imgname){
+		//echo "No image loaded...";
 		return -1;
 	}
 		
@@ -41,14 +41,14 @@ function addImage($db, $id){
 	//$actual_image_name = time().$name.".".$ext;
 	$actual_image_name = time().".".$ext;
 	if(move_uploaded_file($tmp, $path.$actual_image_name)){
-		echo "<img src='uploads/".$actual_image_name."' class='preview'>";
+		//echo "<img src='uploads/".$actual_image_name."' class='preview'>";
 	}
 	else{
 		echo "failed";
 		return -2;
 	}
 
-	echo $id." ".$actual_image_name;
+	//echo $id." ".$actual_image_name;
 	$stmt = $db->prepare("INSERT INTO restaurant_image(restaurant_id,img_path) VALUES(:id, :actual_image_name)");
 	$stmt->bindParam(':id',$id,PDO::PARAM_INT);
 	$stmt->bindParam(':actual_image_name',$actual_image_name,PDO::PARAM_STR);
@@ -58,13 +58,39 @@ function addImage($db, $id){
 if (!isset($db))
 	include_once('connection.php');
 
-if (!isset($id))
-	$id = NULL;
+if (!isset($id)){
+	if (isset($_POST['id']))
+		$id = $_POST['id'];
+	else if(isset($name) || isset($_POST['name'])){
+		if(!isset($name)){
+			$name = $_POST['name'];
+		}
+		$stmt = $db->prepare("Select * From restaurant where name=:name");
+		$stmt->bindParam(':name',$name,PDO::PARAM_STR);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		
+		if(count($result) == 0){
+			echo "Error: Restaurant doesn't exists.";
+			return -3;
+		}
+		
+		else if (count($result) == 1)
+			$id = $result[0]['id'];
+		
+		else{
+			echo "Error: More than one restaurant found with that name";
+			return -3;
+		}
+	}
 
-
-if (isset($_FILES['photo']['name'])){
-	if (isset($_FILES['photo']['name']))
-	addImage($db, $id);
+	else{
+		echo "Error: No way to find restaurant";
+		return -3;
+	}
 }
+
+addImage($db, $id);
+
 
 ?>
