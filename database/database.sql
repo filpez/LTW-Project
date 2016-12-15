952c9a570633eee26f2dae4cdebb137bab9de8c4
Diogo Duque <diogoduque13@hotmail.com>
2016-12-15 16:31:45 +0000

    database.sql being edited for final fixes (db still not updated!)

--- database/database.sql
+++ database/database.sql
@@ -35,26 +35,23 @@ CREATE TABLE review  (
 
 CREATE TABLE comment  (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	owner_id INTEGER REFERENCES user,
-	review_id INTEGER REFERENCES user,
+	review_id INTEGER REFERENCES review,
+	reviewer_id INTEGER REFERENCES user,
 	comment VARCHAR
 );
 
 /* INSERTS */
 
-INSERT INTO user(username, name, password, email, date_created) VALUES('Admin1', 'AdminName','d882b8721a224d38ebb54559e6b54e5df3a1bc6d', 'admin@admin.adm', '2016-12-09 09:09:40'); /*pass=AdminPassword*/
-INSERT INTO user(username, name, password, email, date_created) VALUES('user01','foo','5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8','taa@mail.com', '2016-12-09 09:17:01'); /*password*/
+INSERT INTO user(username, name, password, email, date_created) VALUES('Admin1', 'AdminLOL','d882b8721a224d38ebb54559e6b54e5df3a1bc6d', 'admin@admin.adm', '2016-12-09 09:09:40'); /*pass=AdminPassword*/
+INSERT INTO user(username, name, password, email, date_created) VALUES('user01', 'foo', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'taa@mail.com', '2016-12-09 09:17:01'); /*pass=password*/
+INSERT INTO user(username, name, password, email, date_created) VALUES('user02', 'DJ', '789b49606c321c8cf228d17942608eff0ccc4171', 'user02@yap.uk', '2016-12-14 00:06:21'); /*pass=pass1234*/
 
-INSERT INTO owner(id) VALUES(1);
-INSERT INTO reviewer(id) VALUES(2);
-INSERT INTO reviewer(id) VALUES(3);
-
-INSERT INTO restaurant(owner_id,name,local,description,opening_hours,closing_hours) VALUES(1,'Pidgey na Brasa','Pallet Town','Um belo sitio, onde pode comer todo o tipo de Pidgey a um otimo preco.','19:00','00:00');
-INSERT INTO restaurant(owner_id,name,local,description,opening_hours,closing_hours) VALUES(1,'Krusty Krabby','Celadon City','O melhor Krabby do Mercado!!!','10:00','22:00');
-INSERT INTO restaurant(owner_id,name,local,description,opening_hours,closing_hours) VALUES(1,'McGolduck''s','Safari Zone','Nao e McDonald''s','12:00','01:00');
+INSERT INTO restaurant(owner_id,name,local,description,opening_hours,closing_hours) VALUES(1,'Grilled Pidgey','Pallet Town','A nice place to eat every kind of Pidgey for a great price.','19:00','00:00');
+INSERT INTO restaurant(owner_id,name,local,description,opening_hours,closing_hours) VALUES(1,'Krusty Krabby','Celadon City','Best Krabby in the market!!!','10:00','22:00');
+INSERT INTO restaurant(owner_id,name,local,description,opening_hours,closing_hours) VALUES(2,'McGolduck''s','Safari Zone','Not McDonald''s','7:45','01:00');
+INSERT INTO restaurant(owner_id,name,local,description) VALUE(2, 'Ludicolo''s Tacos', 'Mexico, Mexico', 'BEST. TACOS. EVER');
 
 INSERT INTO review(reviewer_id,restaurant_id,points) VALUES(2,1,5);
 INSERT INTO review(reviewer_id,restaurant_id,points) VALUES(3,1,10);
 
-INSERT INTO restaurant_image (restaurant_id, img_path) VALUES(1,'1480673678AAAAAAAA.jpg');
 
